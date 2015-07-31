0f1d57441f06b202b14e697a4870e493d23021c4
hyfu <71806228>
2015-07-31 18:13:02 +0800

    修改S3资源加载格式。

--- src/cg/data/resource/serverResource/AWSS3ResourceLoader.java
+++ src/cg/data/resource/serverResource/AWSS3ResourceLoader.java
@@ -12,10 +12,10 @@ public class AWSS3ResourceLoader implements ServerResourceLoader {
 	@Override
 	public void load(String serverPath, SingleResourceLoader singleResourceLoader) throws Exception {
 		String[] infos = serverPath.split("\\/");
-		ObjectListing objectListing = AWSS3.listObjects(infos[0], infos[1] + "/");
+		ObjectListing objectListing = AWSS3.listObjects(infos[0].split("\\.")[0], infos[1] + "/");
 		for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
 		    if (objectSummary.getSize() > 0) {
-		    	URI uri = new URI(AWSS3.SCHEME_S3 + "://" + infos[0] + objectSummary.getKey());
+		    	URI uri = new URI(AWSS3.SCHEME_S3 + "://" + infos[0] + "/" + objectSummary.getKey());
 		    	singleResourceLoader.load(uri);
 		    }
 		}
